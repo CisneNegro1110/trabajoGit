e92ac1ad6b327d734351bedcc3690a1782d8fb21
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-02-01 09:13:36 -0700

    Initial Commit

---
+++ Shared/trabajoGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  trabajoGitApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 01/02/22.
+//
+
+import SwiftUI
+
+@main
+struct trabajoGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
